d448faf1a07eeb4e6de3bc6223dd8acb3f3023a3
Yair Lifshitz <yair@lifshitz.io>
2024-11-13 11:16:56 +0200

    1. Remove some unicode formatting characters from transcription results. 2. Black.

--- app.py
+++ app.py
@@ -22,57 +22,56 @@ import ffmpeg
 
 dotenv.load_dotenv()
 
-in_dev = 'TS_STAGING_MODE' in os.environ
+in_dev = "TS_STAGING_MODE" in os.environ
 
 app = Flask(__name__)
 app.config["MAX_CONTENT_LENGTH"] = 250 * 1024 * 1024  # 250MB max file size
-app.secret_key = os.environ['FLASK_APP_SECRET']
+app.secret_key = os.environ["FLASK_APP_SECRET"]
 
 # Configure logging
-logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(message)s')
+logging.basicConfig(level=logging.INFO, format="[%(asctime)s] %(message)s")
 logger = logging.getLogger(__name__)
 
 # Configure file logging
 file_handler = RotatingFileHandler(
-    filename='app.log',
-    maxBytes=10*1024*1024,  # 10MB
-    backupCount=5,
-    encoding='utf-8'
+    filename="app.log", maxBytes=10 * 1024 * 1024, backupCount=5, encoding="utf-8"  # 10MB
 )
-file_handler.setFormatter(logging.Formatter('[%(asctime)s] %(message)s'))
+file_handler.setFormatter(logging.Formatter("[%(asctime)s] %(message)s"))
 logger.addHandler(file_handler)
 
 verbose = "VERBOSE" in os.environ
 
+
 def log_message_in_session(message):
-    user_email = session.get('user_email')
-    if user_email: 
+    user_email = session.get("user_email")
+    if user_email:
         logger.info(f"{user_email}: {message}")
     else:
         logger.info(f"{message}")
 
+
 def log_message(message):
     logger.info(f"{message}")
+
 
 # Configure Google OAuth
 oauth = OAuth(app)
 google = oauth.remote_app(
-    'google',
-    consumer_key=os.environ['GOOGLE_CLIENT_ID'],
-    consumer_secret=os.environ['GOOGLE_CLIENT_SECRET'],
-    request_token_params={
-        'scope': 'email'
-    },
-    base_url='https://www.googleapis.com/oauth2/v1/',
+    "google",
+    consumer_key=os.environ["GOOGLE_CLIENT_ID"],
+    consumer_secret=os.environ["GOOGLE_CLIENT_SECRET"],
+    request_token_params={"scope": "email"},
+    base_url="https://www.googleapis.com/oauth2/v1/",
     request_token_url=None,
-    access_token_method='POST',
-    access_token_url='https://accounts.google.com/o/oauth2/token',
-    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    access_token_method="POST",
+    access_token_url="https://accounts.google.com/o/oauth2/token",
+    authorize_url="https://accounts.google.com/o/oauth2/auth",
 )
 
 ph = None
 if "POSTHOG_API_KEY" in os.environ:
     ph = posthog.Posthog(project_api_key=os.environ["POSTHOG_API_KEY"], host="https://us.i.posthog.com")
+
 
 def capture_event(distinct_id, event, props=None):
     global ph
@@ -84,6 +83,7 @@ def capture_event(distinct_id, event, props=None):
     props["source"] = "transcribe.ivrit.ai"
 
     ph.capture(distinct_id=distinct_id, event=event, properties=props)
+
 
 MAX_PARALLEL_SHORT_JOBS = 2
 MAX_PARALLEL_LONG_JOBS = 1
@@ -128,19 +128,23 @@ ffmpeg_supported_mimes = [
     "application/mxf",
 ]
 
+
 def is_ffmpeg_supported_mimetype(file_mime):
     return any(file_mime.startswith(supported_mime) for supported_mime in ffmpeg_supported_mimes)
+
 
 def get_media_duration(file_path):
     try:
         probe = ffmpeg.probe(file_path)
-        audio_info = next(s for s in probe['streams'] if s['codec_type'] == 'audio')
-        return float(audio_info['duration'])
+        audio_info = next(s for s in probe["streams"] if s["codec_type"] == "audio")
+        return float(audio_info["duration"])
     except ffmpeg.Error as e:
         print(f"Error: {e.stderr}")
         return None
 
+
 SPEEDUP_FACTOR = 5
+
 
 def queue_job(job_id, user_email, filename, duration):
     # Try to add the job to the queue
@@ -149,7 +153,10 @@ def queue_job(job_id, user_email, filename, duration):
     with lock:
         # Check if user already has a job queued or running
         if user_email in user_jobs:
-            return False, (jsonify({"error": "יש לך כבר עבודה בתור או בביצוע. אנא המתן לסיומה לפני העלאת קובץ חדש."}), 400)
+            return False, (
+                jsonify({"error": "יש לך כבר עבודה בתור או בביצוע. אנא המתן לסיומה לפני העלאת קובץ חדש."}),
+                400,
+            )
 
         try:
             job_desc = box.Box()
@@ -188,9 +195,13 @@ def queue_job(job_id, user_email, filename, duration):
 
             capture_event(job_id, "job-queued", {"queue-depth": queue_depth, "job-type": job_type})
 
-            log_message_in_session(f"Job queued successfully: {job_id}, queue depth: {queue_depth}, job type: {job_type}, job desc: {job_desc}")
+            log_message_in_session(
+                f"Job queued successfully: {job_id}, queue depth: {queue_depth}, job type: {job_type}, job desc: {job_desc}"
+            )
 
-            return True, (jsonify({"job_id": job_id, "queued": queue_depth, "job_type": job_type, "time_ahead": time_ahead_str}))
+            return True, (
+                jsonify({"job_id": job_id, "queued": queue_depth, "job_type": job_type, "time_ahead": time_ahead_str})
+            )
         except queue.Full:
             capture_event(job_id, "job-queue-failed", {"queue-depth": queue_depth, "job-type": job_type})
 
@@ -199,51 +210,55 @@ def queue_job(job_id, user_email, filename, duration):
             cleanup_temp_file(job_id)
             return False, (jsonify({"error": "השרת עמוס כרגע. אנא נסה שוב מאוחר יותר."}), 503)
 
+
 @app.route("/")
 def index():
     if in_dev:
-        session['user_email'] = os.environ['TS_USER_EMAIL']
+        session["user_email"] = os.environ["TS_USER_EMAIL"]
 
-    if 'user_email' not in session:
-        return redirect(url_for('login'))
+    if "user_email" not in session:
+        return redirect(url_for("login"))
 
     return render_template("index.html")
 
-@app.route('/login')
+
+@app.route("/login")
 def login():
-    return render_template('login.html',
-                           google_analytics_tag=os.environ['GOOGLE_ANALYTICS_TAG'])
+    return render_template("login.html", google_analytics_tag=os.environ["GOOGLE_ANALYTICS_TAG"])
 
-@app.route('/authorize')
+
+@app.route("/authorize")
 def authorize():
-    return google.authorize(callback=url_for('authorized', _external=True))
+    return google.authorize(callback=url_for("authorized", _external=True))
 
-@app.route('/login/authorized')
+
+@app.route("/login/authorized")
 def authorized():
     resp = google.authorized_response()
-    if resp is None or resp.get('access_token') is None:
-        error_message = 'Access denied: reason={0} error={1}'.format(
-            request.args.get('error_reason', 'Unknown'),
-            request.args.get('error_description', 'Unknown')
+    if resp is None or resp.get("access_token") is None:
+        error_message = "Access denied: reason={0} error={1}".format(
+            request.args.get("error_reason", "Unknown"), request.args.get("error_description", "Unknown")
         )
-        return render_template('close_window.html', success=False, message=error_message)
+        return render_template("close_window.html", success=False, message=error_message)
 
-    session['google_token'] = (resp['access_token'], '')
-    user_info = google.get('userinfo')
-    session['user_email'] = user_info.data["email"]
+    session["google_token"] = (resp["access_token"], "")
+    user_info = google.get("userinfo")
+    session["user_email"] = user_info.data["email"]
 
-    session.pop('google_token')
+    session.pop("google_token")
 
-    return render_template('close_window.html', success=True)
+    return render_template("close_window.html", success=True)
+
 
 @google.tokengetter
 def get_google_oauth_token():
-    return session.get('google_token')
+    return session.get("google_token")
+
 
 @app.route("/upload", methods=["POST"])
 def upload_file():
     job_id = str(uuid.uuid4())
-    user_email = session.get('user_email')
+    user_email = session.get("user_email")
 
     capture_event(job_id, "file-upload")
 
@@ -287,15 +302,12 @@ def upload_file():
 
         queued, res = queue_job(job_id, user_email, filename, duration)
         if queued:
-            job_results[job_id] = {
-                'results': [],
-                'completion_time': None,
-                'progress': 0
-            }
+            job_results[job_id] = {"results": [], "completion_time": None, "progress": 0}
         else:
             cleanup_temp_file(job_id)
 
     return res
+
 
 @app.route("/job_status/<job_id>")
 def job_status(job_id):
@@ -311,7 +323,7 @@ def job_status(job_id):
         queue_position = None
         time_ahead = 0
         job_type = None
-        
+
         # Check short queue
         for idx, job_desc in enumerate(short_job_queue.queue):
             if job_desc.id == job_id:
@@ -336,21 +348,22 @@ def job_status(job_id):
                 time_ahead += sum(job.duration for job in running_short_jobs.values())
             else:
                 time_ahead += sum(job.duration for job in running_long_jobs.values())
-            
+
             # Apply speedup factor
             time_ahead /= SPEEDUP_FACTOR
-            
+
             # Convert time_ahead to HH:MM:SS format
             time_ahead_str = str(timedelta(seconds=int(time_ahead)))
-            
-            return jsonify({'queue_position': queue_position, 'time_ahead': time_ahead_str, 'job_type': job_type})
+
+            return jsonify({"queue_position": queue_position, "time_ahead": time_ahead_str, "job_type": job_type})
 
         # If job is in progress, return only the progress
-        if job_results[job_id]['progress'] < 1.0:
-            return jsonify({'progress': job_results[job_id]['progress']})
+        if job_results[job_id]["progress"] < 1.0:
+            return jsonify({"progress": job_results[job_id]["progress"]})
 
         # If job is complete, return the full job_result data
         return jsonify(job_results[job_id])
+
 
 def cleanup_temp_file(job_id):
     if job_id in temp_files:
@@ -361,6 +374,27 @@ def cleanup_temp_file(job_id):
             log_message(f"Error deleting temporary file: {str(e)}")
         finally:
             del temp_files[job_id]
+
+
+def clean_some_unicode_from_text(text):
+    # Using ranges to be more complete
+    return text.translate(
+        {
+            ord(c): None
+            for c in (
+                # Bidirectional formatting
+                "\u200E\u200F"  # LTR and RTL marks
+                "\u202A-\u202E"  # LTR/RTL embedding, pop, override
+                # Zero-width characters
+                "\u200B-\u200D"  # Zero-width space, non-joiner, joiner
+                "\uFEFF"  # Zero-width no-break space
+                # Other directional formatting
+                "\u061C"  # Arabic letter mark
+                "\u2066-\u2069"  # Isolate controls
+            )
+        }
+    )
+
 
 def transcribe_job(job_desc):
     job_id = job_desc.id
@@ -389,7 +423,7 @@ def transcribe_job(job_desc):
                 "id": seg.id,
                 "start": seg.start,
                 "end": seg.end,
-                "text": seg.text,
+                "text": clean_some_unicode_from_text(seg.text),
                 "avg_logprob": seg.avg_logprob,
                 "compression_ratio": seg.compression_ratio,
                 "no_speech_prob": seg.no_speech_prob,
@@ -397,8 +431,8 @@ def transcribe_job(job_desc):
 
             progress = seg.end / duration
             with lock:
-                job_results[job_id]['progress'] = progress
-                job_results[job_id]['results'].append(segment)
+                job_results[job_id]["progress"] = progress
+                job_results[job_id]["results"].append(segment)
 
         log_message(f"{job_desc.user_email}: done transcribing job {job_id}, audio duration was {duration}.")
 
@@ -410,8 +444,8 @@ def transcribe_job(job_desc):
         )
 
         with lock:
-            job_results[job_id]['progress'] = 1.0
-            job_results[job_id]['completion_time'] = datetime.now()
+            job_results[job_id]["progress"] = 1.0
+            job_results[job_id]["completion_time"] = datetime.now()
     finally:
         if job_desc.duration <= SHORT_JOB_THRESHOLD:
             del running_short_jobs[job_id]
@@ -426,6 +460,7 @@ def transcribe_job(job_desc):
         if job_id in job_threads:
             del job_threads[job_id]
 
+
 def submit_next_task(job_queue, running_jobs, max_parallel_jobs):
     with lock:
         if len(running_jobs) < max_parallel_jobs:
@@ -436,16 +471,18 @@ def submit_next_task(job_queue, running_jobs, max_parallel_jobs):
                 job_threads[job_desc.id] = t_thread
                 t_thread.start()
 
+
 def cleanup_old_results():
     with lock:
         current_time = datetime.now()
         jobs_to_delete = []
         for job_id, job_data in job_results.items():
-            if job_data['completion_time'] and (current_time - job_data['completion_time']) > timedelta(minutes=30):
+            if job_data["completion_time"] and (current_time - job_data["completion_time"]) > timedelta(minutes=30):
                 jobs_to_delete.append(job_id)
 
         for job_id in jobs_to_delete:
             del job_results[job_id]
+
 
 def terminate_inactive_jobs():
     with lock:
@@ -476,6 +513,7 @@ def terminate_inactive_jobs():
 
             # The job thread will terminate itself in the next iteration of the transcribe_job function
 
+
 def event_loop():
     while True:
         submit_next_task(short_job_queue, running_short_jobs, MAX_PARALLEL_SHORT_JOBS)
@@ -483,6 +521,7 @@ def event_loop():
         cleanup_old_results()
         terminate_inactive_jobs()
         time.sleep(0.1)
+
 
 el_thread = threading.Thread(target=event_loop)
 el_thread.start()
